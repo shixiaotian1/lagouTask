ffbdc502dfedaf58966ceaab97e91935c13b38e1
18030196 <shixiaotian@shinho.net.cn>
2020-04-11 00:37:59 +0800

    作业一基础框架提交

---
+++ 第一阶段模块三作业/mvc/src/main/java/com/lagou/demo/controller/DemoController.java
@@ -0,0 +1,45 @@
+package com.lagou.demo.controller;
+
+import com.lagou.demo.service.IDemoService;
+import com.lagou.edu.mvcframework.annotations.LagouAutowired;
+import com.lagou.edu.mvcframework.annotations.LagouController;
+import com.lagou.edu.mvcframework.annotations.LagouRequestMapping;
+import com.lagou.edu.mvcframework.annotations.Security;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@LagouController
+@LagouRequestMapping("/demo")
+public class DemoController {
+
+
+    @LagouAutowired
+    private IDemoService demoService;
+
+
+    /**
+     * URL: /demo/query?name=lisi
+     * @param request
+     * @param response
+     * @param name
+     * @return
+     */
+    @LagouRequestMapping("/query/success")
+    @Security("zhangsan")
+    public String querySuccess(HttpServletRequest request, HttpServletResponse response,String name) {
+        return demoService.get(name);
+    }
+
+    /**
+     * URL: /demo/query?name=lisi
+     * @param request
+     * @param response
+     * @param name
+     * @return
+     */
+    @LagouRequestMapping("/query/fail")
+    public String queryFail(HttpServletRequest request, HttpServletResponse response,String name) {
+        return demoService.get(name);
+    }
+}
